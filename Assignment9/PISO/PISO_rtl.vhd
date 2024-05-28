b63420a4c92bcac58d4b504c7114649029aa974f
Subhadeep Roy <subha9.5roy350@gmail.com>
2024-05-28 21:07:13 +0530

    Update PISO_rtl.vhd

--- Assignment9/PISO/PISO_rtl.vhd
+++ Assignment9/PISO/PISO_rtl.vhd
@@ -2,40 +2,46 @@
 ----------------------------------------------------------------------------------
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-entity PISO_rtl is
+
+entity piso_rtl is
     Port ( Data : in  STD_LOGIC_VECTOR (3 downto 0);
            Rst : in  STD_LOGIC;
            CLK : in  STD_LOGIC;
            Load : in  STD_LOGIC;
+           Dout_vector : out  STD_LOGIC_VECTOR (3 downto 0);
            Dout : out  STD_LOGIC);
-end PISO_rtl;
+end piso_rtl;
 
-architecture Behavioral of PISO_rtl is
-component d_ff_rtl is
+architecture Behavioral of piso_rtl is
+
+component d_rtl is
     Port ( D : in  STD_LOGIC;
            Reset : in  STD_LOGIC;
-           CLK : in  STD_LOGIC;
+           Clk : in  STD_LOGIC;
            Q : out  STD_LOGIC;
            QBar : out  STD_LOGIC);
 end component;
-component mux_rtl is
-    Port ( A : in  STD_LOGIC;
-           B : in  STD_LOGIC;
-           C : in  STD_LOGIC;
-           D : in  STD_LOGIC;
-           Y : out  STD_LOGIC);
-end component;
-signal t0,t1,t2,t3,t4,t5,t6: std_logic:='0';
+
+signal q0, q1, q2, q3, t1, t2, t3:STD_LOGIC:='0';
+
 begin
-D0: d_ff_rtl port map(D=>Data(0),CLK=>CLK,Reset=>Rst,Q=>t0);
-M0: mux_rtl port map(A=>t0,B=>Load,C=>Load,D=>Data(1),Y=>t1);
 
-D1: d_ff_rtl port map(D=>t1,CLK=>CLK,Reset=>Rst,Q=>t2);
-M1: mux_rtl port map(A=>t2,B=>Load,C=>Load,D=>Data(2),Y=>t3);
+d0 : d_rtl port map (D => Data(0), Clk => CLK, Reset => Rst, Q => q0);
+Dout_vector(0) <= q0;
+t1 <= (Load and q0) or ((not Load) and Data(1));
 
-D2: d_ff_rtl port map(D=>t3,CLK=>CLK,Reset=>Rst,Q=>t4);
-M2: mux_rtl port map(A=>t4,B=>Load,C=>Load,D=>Data(3),Y=>t5);
+d1 : d_rtl port map (D => t1, Clk => CLK, Reset => Rst, Q => q1);
+Dout_vector(1) <= q1;
+t2 <= (Load and q1) or ((not Load) and Data(2));
 
-D3: d_ff_rtl port map(D=>t5,CLK=>CLK,Reset=>rst,Q=>Dout);
+d2 : d_rtl port map (D => t2, Clk => CLK, Reset => Rst, Q => q2);
+Dout_vector(2) <= q2;
+t3 <= (Load and q2) or ((not Load) and Data(3));
+
+d3 : d_rtl port map (D => t3, Clk => CLK, Reset => Rst, Q => q3);
+Dout <= q3;
+Dout_vector(3) <= q3;
+
+
 end Behavioral;
 
